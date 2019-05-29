7691a82ccc1c396d50f87fd6547924d1d78d4d4b
David Ludwig <davidludwigii@gmail.com>
2019-05-29 17:24:35 -0500

    Cubitick now works on servers

--- src/main/java/net/minecraftforge/server/cubitick/Cubitick.java
+++ src/main/java/net/minecraftforge/server/cubitick/Cubitick.java
@@ -1,36 +1,67 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2018.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.server.cubitick;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Timer;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.server.cubitick.network.CubitickMessage;
+import net.minecraftforge.server.cubitick.network.CubitickMessageHandler;
 
 public class Cubitick {
-
     public static Cubitick instance = new Cubitick();
+
     public static boolean initialised = false;
 
-    private String author = "Cubitect";
-    private String name = "Cubitick";
-    private String versionName = "[1.10]v1.5.2";
-    private String versionType = "main";
-    private String mcVersion   = "1.10";
-    private String mcType      = "release";
-
     public static final float tickrate = 20F;
-    public static float tickrateWorld = 20F;
-    public static boolean synctick = true;
+    public static float tickrateWorld  = 20F;
+    public static boolean synctick     = false;
 
+    public static SimpleNetworkWrapper NETWORK;
 
     public Cubitick()
     {
         initialised = false;
+        NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel("Cubitick");
+        NETWORK.registerMessage(CubitickMessageHandler.class, CubitickMessage.class, 0, Side.CLIENT);
+        NETWORK.registerMessage(CubitickMessageHandler.class, CubitickMessage.class, 1, Side.SERVER);
     }
 
-    public static void setTickWorld(float rate)
+    public static void setTickWorldClient(float rate)
     {
+        System.out.println("Setting Client Tickrate:" + rate);
         tickrateWorld = rate;
         setTimerWorld(rate);
     }
 
+    public static void setTickWorldServer(float rate)
+    {
+        System.out.println("Setting Server Tickrate: " + rate);
+        tickrateWorld = rate;
+        changeClientTickrate();
+    }
+
+    @SideOnly(Side.CLIENT)
     private static void setTimerWorld(float rate)
     {
         Minecraft mc = Minecraft.getMinecraft();
@@ -49,13 +80,20 @@ public class Cubitick {
 
     public static float getTickms()
     {
-        if(Cubitick.tickrateWorld <= 0) return Float.MAX_VALUE;
+        if (Cubitick.tickrateWorld <= 0) return Float.MAX_VALUE;
         return 1000F/Cubitick.tickrateWorld;
     }
 
     public static void playerChat(String str)
     {
-        if(Minecraft.getMinecraft().player == null) return;
+        if (Minecraft.getMinecraft().player == null) return;
         Minecraft.getMinecraft().player.sendChatMessage(str);
+    }
+
+    public static void changeClientTickrate()
+    {
+        try {
+            NETWORK.sendToAll(new CubitickMessage(tickrateWorld, synctick));
+        } catch(NullPointerException e) {}
     }
 }
